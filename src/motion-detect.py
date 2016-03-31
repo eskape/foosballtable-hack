338eeb73d8fc054cad07ba52db551d055500d4d4
Rik de Groot <hwdegroot@gmail.com>
2016-03-31 13:59:29 +0200

    Make idle mode less verbose

--- src/motion-detect.py
+++ src/motion-detect.py
@@ -9,7 +9,7 @@ while True:
   i=GPIO.input(11)
   if i==0:
     #When output from motion sensor is LOW
-    print "No intruders",i
+    #print "No intruders",i
     GPIO.output(3, 0)  #Turn OFF LED
     time.sleep(0.1)
   elif i==1:               #When output from motion sensor is HIGH
