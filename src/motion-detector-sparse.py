a9c7a97017e4f12729f08bed534ac3f59baa1c7d
Rik de Groot <hwdegroot@gmail.com>
2016-03-31 15:36:38 +0200

    Simple motion detector script.

---
+++ src/motion-detector-sparse.py
@@ -0,0 +1,15 @@
+#!/bin/python
+
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+pir = 40
+GPIO.setup(pir, GPIO.IN)
+print "Waiting for sensor to settle"
+time.sleep(2)
+print "Detecting motion"
+while True:
+    if GPIO.input(pir):
+        print "Motion Detected!"
+       time.sleep(2)
+       time.sleep(0.1)
