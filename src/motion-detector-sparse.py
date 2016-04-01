5ed02f7c38e9747729801d5753bfeee3bc33b4b5
Rik de Groot <rik.de.groot@mendix.com>
2016-04-01 13:15:28 +0200

    Use events

--- src/motion-detector-sparse.py
+++ src/motion-detector-sparse.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 port_a = 40
-port_b = 33
+port_b = 37
 GPIO.setup(port_a, GPIO.IN)
 GPIO.setup(port_b, GPIO.IN)
 print "Waiting for sensor to settle"
@@ -14,5 +14,5 @@ print "Detecting motion"
 GPIO.add_event_detect(port_a, GPIO.RISING, callback=gimme_motion)
 GPIO.add_event_detect(port_b, GPIO.RISING, callback=gimme_motion)
 
-def gimme_motion(channel, port):
-  print "A: Motion Detected! @ channel %d" % channel
+def gimme_motion(channel):
+  print "CH%d Motion Detected!" % channel
