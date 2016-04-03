ec3db5aa178314b20c3e9b3f5e8ef9b28c223e82
Rik de Groot <hwdegroot@gmail.com>
2016-04-03 06:01:39 +0200

    Give teams actual names

--- src/track-scores.py
+++ src/track-scores.py
@@ -10,8 +10,8 @@ port_a = 40
 port_b = 37
 
 motion_detector = MotionDetector(idle_timeout = 5)
-motion_detector.add_listener(port_a)
-motion_detector.add_listener(port_b)
+motion_detector.add_listener(port_a, "TeamA")
+motion_detector.add_listener(port_b, "Team B")
 
 
 global user_input
