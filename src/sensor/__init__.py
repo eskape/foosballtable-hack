40c363e8752b7d1bcc1396c38f8130f68db385e6
Kishen Simbhoedatpanday <eskapenl@gmail.com>
2016-07-29 11:52:48 +0200

    using try catch for error handling. using real foosballtable app to send scores

--- src/sensor/__init__.py
+++ src/sensor/__init__.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import logging
 import requests
+import json
 
 class Sensor:
   def __init__(self, *channels):
@@ -16,16 +17,16 @@ class Sensor:
       number += 1
       GPIO.setup(channel, GPIO.IN)
       GPIO.add_event_detect(channel, GPIO.FALLING, callback=self.__teamscores)
-      self.__mappings[channel] = "Team %d" % number
+      self.__mappings[channel] = number
       logging.info("Listening for events for %s on channel %s" % (self.__mappings[channel], channel))
   
   def __teamscores(self, channel):
     logging.info("Event detected for %s on channel %s" % (self.__mappings[channel], channel))
-    response = requests.post("http://httpbin.org/post", data = {"Team": self.__mappings[channel]});
-
-    if (response.status_code == 200):
+    response = requests.post("http://foosballtable.mxapps.io/rest/event?Team=%s&Delta=1" % self.__mappings[channel]);
+    try:
+      response.raise_for_status()
       logging.info("Sent %s score successfully" % self.__mappings[channel])
-    else:
+    except Exception:
       logging.info("Failed sending %s score" % self.__mappings[channel])
       logging.info("Status code %s" % response.status_code)
       logging.info(response.text)
